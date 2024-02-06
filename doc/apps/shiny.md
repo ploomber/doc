43d15202fbc07c09a139cc3cbdb2987a63dc5902
Neelasha Sen <neelasha.sen@gmail.com>
2024-02-06 21:56:53 +0530

    Shiny (Python) ML training example (#105)
    * Shiny core example
    
    readme
    
    zip
    
    removed unused function
    
    shiny doc
    
    removed parallel function
    
    shiny core toc
    
    renamed folder
    
    rename
    
    * reamde changes
    
    * Delete examples/docker/shiny-core/Dockerfile
    
    * removed core files
    
    * env variables
    
    * env variable
    
    * dummy record
    
    * readme
    
    * zip
    
    * polling function
    
    * timestamp error
    
    * timestamp error
    
    * updates instructions
    
    * minor updates
    
    ---------
    
    Co-authored-by: Eduardo Blancas <github@blancas.io>

---
+++ doc/apps/shiny.md
@@ -0,0 +1,43 @@
+# Shiny (Python)
+
+To deploy a [Shiny](https://shiny.posit.co/py/docs/overview.html) Python application to Ploomber Cloud you need:
+
+- A `Dockerfile`
+- A Shiny Python project
+
+## `Dockerfile`
+
+Use this [template](https://github.com/ploomber/doc/blob/main/examples/docker/shiny/Dockerfile) `Dockerfile`:
+
+```Dockerfile
+FROM python:3.11
+
+COPY app.py app.py
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+ENTRYPOINT ["shiny", "run", "app.py", "--host", "0.0.0.0", "--port", "80"]
+```
+
+## Testing locally
+
+To test your app, you can use `docker` locally:
+
+```sh
+# build the docker image
+docker build . -t shiny-app
+
+# run it
+docker run -p 5000:80 shiny-app
+```
+
+Now, open [http://0.0.0.0:5000/](http://0.0.0.0:5000/) to see your app.
+
+
+## Deploy
+
+Once you have all your files, create a zip file.
+
+To deploy a Shiny app from the deployment menu, select the Docker option and follow the instructions:
+
+![](../static/docker.png)
---
+++ examples/docker/shiny/app.py
@@ -0,0 +1,149 @@
+import os
+import pandas as pd
+from datetime import datetime
+
+import plotly.express as px
+from shinywidgets import output_widget, render_widget
+from faicons import icon_svg
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+
+THRESHOLD_MID = 0.9
+THRESHOLD_MID_COLOR = "rgb(0, 137, 26)"
+THRESHOLD_LOW = 0.5
+THRESHOLD_LOW_COLOR = "rgb(193, 0, 0)"
+
+URI = os.environ.get("MONGODB_CONNECTION_URI", "")
+
+# Create a new client and connect to the server
+client = MongoClient(URI, server_api=ServerApi("1"))
+
+try:
+    client.admin.command("ping")
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    raise ConnectionError(str(e)) from e
+
+db = client.myDatabase
+my_collection = db["accuracy_scores"]
+
+
+def number_of_observations():
+    n = my_collection.count_documents({})
+    if n == 0:
+        # insert random record to prevent KeyError message when no records present in DB
+        my_collection.insert_one({"model": "", "score": 0, "timestamp": datetime.now()})
+    return n
+
+
+@reactive.poll(lambda: number_of_observations())
+def df():
+    """
+    @reactive.poll calls a cheap query (`last_modified()`) every 1 second to check if
+    the expensive query (`df()`) should be run and downstream calculations should be
+    updated.
+    """
+    results = my_collection.find().sort({"timestamp": -1}).limit(50)
+    tbl = pd.DataFrame(list(results))
+    tbl["time"] = tbl["timestamp"]
+    # Reverse order of rows
+    tbl = tbl.iloc[::-1]
+    return tbl
+
+
+model_names = ["model_1", "model_2"]
+model_colors = {
+    name: color
+    for name, color in zip(model_names, px.colors.qualitative.D3[0 : len(model_names)])
+}
+
+
+def make_value_box(model, score):
+    theme = "text-success"
+    icon = icon_svg("check", width="50px")
+    if score < THRESHOLD_MID:
+        theme = "text-warning"
+        icon = icon_svg("triangle-exclamation", width="50px")
+    if score < THRESHOLD_LOW:
+        theme = "bg-danger"
+        icon = icon_svg("circle-exclamation", width="50px")
+
+    return ui.value_box(model, ui.h2(score), theme=theme, showcase=icon)
+
+
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.input_checkbox_group("models", "Models", model_names, selected=model_names),
+    ),
+    ui.row(
+        ui.h1("Model monitoring dashboard"),
+        ui.h4("Deploy your own at ploomber.io"),
+        ui.output_ui("value_boxes"),
+    ),
+    ui.row(
+        ui.card(output_widget("plot_timeseries")),
+    ),
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @reactive.Calc
+    def filtered_df():
+        """
+        Return the data frame that should be displayed in the app, based on the user's
+        input. This will be either the latest rows, or a specific time range. Also
+        filter out rows for models that the user has deselected.
+        """
+        data = df()
+
+        # Filter the rows so we only include the desired models
+        return data[data["model"].isin(input.models())]
+
+    @reactive.Calc
+    def filtered_model_names():
+        return filtered_df()["model"].unique()
+
+    @output
+    @render.ui
+    def value_boxes():
+        data = filtered_df()
+        models = data["model"].unique().tolist()
+        scores_by_model = {
+            x: data[data["model"] == x].iloc[-1]["score"] for x in models
+        }
+        # Round scores to 2 decimal places
+        scores_by_model = {x: round(y, 2) for x, y in scores_by_model.items()}
+
+        return ui.layout_columns(
+            *[
+                # For each model, return a value_box with the score, colored based on
+                # how high the score is.
+                make_value_box(model, score)
+                for model, score in scores_by_model.items()
+            ],
+            width="135px",
+        )
+
+    @render_widget
+    def plot_timeseries():
+        """
+        Returns a Plotly Figure visualization. Streams new data to the Plotly widget in
+        the browser whenever filtered_df() updates, and completely recreates the figure
+        when filtered_model_names() changes (see recreate_key=... above).
+        """
+        fig = px.line(
+            filtered_df(),
+            x="time",
+            y="score",
+            labels=dict(score="accuracy"),
+            color="model",
+            color_discrete_map=model_colors,
+            template="simple_white",
+        )
+
+        return fig
+
+
+app = App(app_ui, server)
