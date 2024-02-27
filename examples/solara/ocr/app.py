89f132f3c1f1acfe46d0052d33a968a6613366d1
Bryan Ho <bryannho@usc.edu>
2024-02-26 16:11:36 -0800

    solara, remove dockerfiles

---
+++ examples/solara/ocr/app.py
@@ -0,0 +1,87 @@
+import requests
+import solara
+import solara.lab
+from solara.components.file_drop import FileDrop, FileInfo
+from solara.alias import rv
+import easyocr
+from pathlib import Path
+
+
+reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
+
+
+class State:
+    image = solara.reactive(None)
+    new_image_loaded = solara.reactive(False)
+    extraction_complete = solara.reactive(False)
+    extracted_text = solara.reactive("")
+
+    @staticmethod
+    def load_sample():
+        image_path = Path("label.png")
+        State.image.value = image_path.read_bytes()
+        State.new_image_loaded.value = True
+        State.extracted_text.value = reader.readtext(State.image.value, detail=0)
+        State.extraction_complete.value = True
+        
+    @staticmethod
+    def load_from_file(file):
+        State.extraction_complete.value = False
+        State.image.value = file["file_obj"].read()
+        State.new_image_loaded.value = True
+        State.extracted_text.value = reader.readtext(State.image.value, detail=0)
+        State.extraction_complete.value = True
+
+    @staticmethod
+    def reset():
+        State.image.value = None
+        State.new_image_loaded.value = False
+        State.extraction_complete.value = False
+        State.extracted_text.value = ""
+
+
+@solara.component
+def Page():
+
+    image = State.image.value
+
+    with solara.AppBarTitle():
+        solara.Text("OCR App")
+
+    with solara.Card(title="About", elevation=6, style="background-color: #f5f5f5;"):
+        solara.Markdown("""This app can extract text from images. 
+        Drop an image in the box to the left to get started or click on \"Sample image\"""")
+
+    with solara.Sidebar():
+        with solara.Card("Controls", margin=0, elevation=0):
+            with solara.Column():
+                with solara.Row():
+                    solara.Button("Sample image", color="primary", text=True, outlined=True, on_click=State.load_sample)
+                FileDrop(on_file=State.load_from_file, label="Drag an image file here")
+
+                solara.Markdown("Hosted in [Ploomber Cloud](https://ploomber.io/)")
+
+    if State.new_image_loaded.value and State.extraction_complete.value == False:
+        with solara.Div():
+            solara.Text("Extracting text...")
+            solara.ProgressLinear(True)
+
+    if State.extraction_complete.value:
+        with solara.HBox():
+            with solara.Card():
+                solara.Image(image, format="jpeg")
+            with solara.Card():
+                if State.extracted_text.value:
+                    solara.HTML(tag="h3", style="margin: auto;", unsafe_innerHTML="Extracted text")
+                    solara.Markdown('\n'.join(State.extracted_text.value))
+                else:
+                    solara.Markdown("No text found")
+
+    if State.image.value is None:
+        solara.Info("No image loaded, click on the sample image button to load a sample image, or upload a file.")
+
+
+@solara.component
+def Layout(children):
+    route, routes = solara.use_route()
+    return solara.AppLayout(children=children)
---
+++ examples/solara/personality-quiz-rag/README.md
@@ -0,0 +1,48 @@
+## How to run this application locally
+
+### 1. Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+### 2. Ensure you have an OpenAI API key and save it into a `.env` file
+
+
+### 3. Run the application
+
+```bash
+solara run app.py
+```
+
+### Navigate to the application
+
+A web browser window should automatically open to the application. If it does not, navigate to http://localhost:8765 in your web browser.
+
+## Deploying this application on Ploomber Cloud
+
+Ensure you have an account and an API key. Refer to the [documentation on creating an account](https://docs.cloud.ploomber.io/en/latest/quickstart/signup.html) and [documentation on generating an API key](https://docs.cloud.ploomber.io/en/latest/quickstart/apikey.html) for more details.
+
+### 1. Install Ploomber Cloud's CLI
+
+```bash
+pip install ploomber-cloud
+```
+
+### 2. Connect your API key
+
+```bash
+ploomber-cloud key YOURKEY
+```
+
+### 3. Initialize your app
+
+```bash
+ploomber-cloud init
+```
+
+### 4. Deploy your app
+
+```bash
+ploomber-cloud deploy
+```
