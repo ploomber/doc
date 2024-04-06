2b6ebef40dc85dca3ced59825ea556236fc074f4
neelasha23 <neelasha.sen@gmail.com>
2024-04-06 19:35:02 +0530

    Dockerfile
    cli

--- examples/chainlit/chat-with-pdf/README.md
+++ examples/chainlit/chat-with-pdf/README.md
@@ -4,6 +4,14 @@ A Chainlit application for chatting with a PDF file.
 
 ![](screenshot.webp)
 
+## Set key
+
+To run this example you need to set the `OPENAI_API_KEY` environment variable.
+
+```bash
+export OPENAI_API_KEY=<your_api_key> 
+```
+
 ## Testing locally
 
 Run the below command to start the app:
@@ -11,3 +19,11 @@ Run the below command to start the app:
 ```bash
 chainlit run app.py 
 ```
+
+## Deployment
+
+* Create a zip of `app.py`, `chainlit.md`, `requirements.txt` and `Dockerfile`.
+* Follow the instructions for deploying a [Chainlit](https://docs.cloud.ploomber.io/en/latest/apps/chainlit.html) application.
+* You also need to set a couple of [environment variables](https://docs.cloud.ploomber.io/en/latest/user-guide/env-vars.html) while deploying the application.
+    * `OPENAI_API_KEY`
+    * `PLOOMBER_CLOUD_KEY`: Refer to [documentation](https://docs.cloud.ploomber.io/en/latest/quickstart/apikey.html)
--- examples/chainlit/chat-with-pdf/app.py
+++ examples/chainlit/chat-with-pdf/app.py
@@ -1,4 +1,3 @@
-import PyPDF2
 from pathlib import Path
 from typing import List
 import shutil
@@ -8,12 +7,12 @@ import lancedb
 from openai import OpenAI
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import LanceDB
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import LanceDB
 from langchain.chains import (
     ConversationalRetrievalChain,
 )
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 
 from langchain.docstore.document import Document
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
@@ -35,7 +34,8 @@ async def on_chat_start():
     # Wait for the user to upload a file
     while files is None:
         files = await cl.AskFileMessage(
-            content="Please upload a PDF file to begin!",
+            content="Please upload a PDF file to begin!\n"
+                    "The processing of the file may require a few moments or minutes to complete",
             accept=["application/pdf"],
             max_size_mb=100,
             timeout=180,
@@ -46,23 +46,14 @@ async def on_chat_start():
     msg = cl.Message(content=f"Processing `{file.name}`...", disable_feedback=True)
     await msg.send()
 
-    # result = None
-    # jobid = functions.pdf_to_text(file.path, block=False)
-    # while not isinstance(result, list):
-    #     try:
-    #         result = functions.get_result(jobid)
-    #     except Exception:
-    #         pass
-    # print(result)
-    # pdf_text = "".join(result)
-
-    # Read the PDF file
-    pdf = PyPDF2.PdfReader(file.path)
-    pdf_text = ""
-    for page in pdf.pages:
-        pdf_text += page.extract_text()
-
-    page_count = int(0.1 * len(pdf.pages))
+    result = None
+    jobid = functions.pdf_to_text(file.path, block=False)
+    while not isinstance(result, list):
+        try:
+            result = functions.get_result(jobid)
+        except Exception:
+            pass
+    pdf_text = "".join(result)
 
     # Split the text into chunks
     texts = text_splitter.split_text(pdf_text)
@@ -100,7 +91,7 @@ async def on_chat_start():
     chain = ConversationalRetrievalChain.from_llm(
         ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
         chain_type="stuff",
-        retriever=docsearch.as_retriever(search_kwargs={"k": min(page_count, 5)}),
+        retriever=docsearch.as_retriever(search_kwargs={"k": 3}),
         memory=memory,
         return_source_documents=True,
     )
