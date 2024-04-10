6eadda48b31b6b7a6c09b4767828c9cc4f96a571
neelasha23 <neelasha.sen@gmail.com>
2024-04-10 17:30:41 +0530

    langhcian updates

--- examples/chainlit/chat-with-pdf/app.py
+++ examples/chainlit/chat-with-pdf/app.py
@@ -1,29 +1,25 @@
 import shutil
 import asyncio
+import pyarrow as pa
 from pathlib import Path
 from typing import List
 
 import lancedb
 import chainlit as cl
-from openai import OpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import LanceDB
-from langchain.chains import (
-    ConversationalRetrievalChain,
-)
-from langchain_community.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain.docstore.document import Document
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import LanceDB
 
 from ploomber_cloud.functions import pdf_to_text, pdf_scanned_to_text, get_result
 
-client = OpenAI()
 embeddings = OpenAIEmbeddings()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 
 async def process_pdf(file, pdf_to_text_func):
+    """Function for extracting text from PDF"""
     jobid = pdf_to_text_func(file.path, block=False)
     result = None
     while not isinstance(result, list):
@@ -31,7 +27,8 @@ async def process_pdf(file, pdf_to_text_func):
             result = get_result(jobid)
         except Exception:
             await asyncio.sleep(1)
-    return "".join(result)
+    result = [text.strip() for text in result if text.strip() != ""]
+    return result
 
 
 @cl.on_chat_start
@@ -54,17 +51,24 @@ async def on_chat_start():
     await msg.send()
 
     pdf_text = await process_pdf(file, pdf_to_text)
-    if not pdf_text:
+
+    if len(pdf_text) == 0:
         msg.content = (
             f"This looks like a scanned document. Reprocessing `{file.name}`..."
         )
         await msg.update()
         pdf_text = await process_pdf(file, pdf_scanned_to_text)
 
-    # Split the text into chunks
-    texts = text_splitter.split_text(pdf_text)
+    # Convert each page into a Document object with metadata
+    documents = [
+        Document(page_content=text, metadata={"source": file.path, "page": ind})
+        for ind, text in enumerate(pdf_text)
+    ]
 
-    if not texts:
+    # Split the text into chunks
+    documents = text_splitter.split_documents(documents)
+
+    if not documents:
         msg.content = (
             f"Couldn't find any text in `{file.name}`. "
             f"Click on `New Chat` for uploading another file."
@@ -72,9 +76,6 @@ async def on_chat_start():
         await msg.update()
 
     else:
-        # Create a metadata for each chunk
-        metadatas = [{"source": f"{i}-pl"} for i in range(len(texts))]
-
         directory_path = Path(file.path).parent
         path_to_vector_db = Path(directory_path, "vector-db")
 
@@ -82,37 +83,27 @@ async def on_chat_start():
             shutil.rmtree(path_to_vector_db)
 
         db = lancedb.connect(path_to_vector_db)
-        table = db.create_table(
-            "pdf",
-            data=[
-                {
-                    "vector": embeddings.embed_query(texts[0]),
-                    "text": texts[0],
-                    "id": "1",
-                }
-            ],
-            mode="overwrite",
+
+        schema = pa.schema(
+            [
+                pa.field("vector", pa.list_(pa.float32(), list_size=1536)),
+                pa.field("text", pa.string()),
+                pa.field("id", pa.string()),
+            ]
         )
 
-        docsearch = LanceDB.from_texts(
-            texts, embeddings, metadatas=metadatas, connection=table
-        )
+        table = db.create_table("embeddings", schema=schema, mode="overwrite")
 
-        message_history = ChatMessageHistory()
+        docsearch = LanceDB.from_documents(documents, embeddings, connection=table)
 
-        memory = ConversationBufferMemory(
-            memory_key="chat_history",
-            output_key="answer",
-            chat_memory=message_history,
-            return_messages=True,
-        )
-
-        chain = ConversationalRetrievalChain.from_llm(
-            ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
+        chain = RetrievalQA.from_chain_type(
+            llm=OpenAI(),
             chain_type="stuff",
-            retriever=docsearch.as_retriever(search_kwargs={"k": 3}),
-            memory=memory,
+            retriever=docsearch.as_retriever(
+                search_type="similarity", search_kwargs={"k": 2}
+            ),
             return_source_documents=True,
+            verbose=True,
         )
 
         # Let the user know that the system is ready
@@ -128,7 +119,8 @@ async def main(message: cl.Message):
     cb = cl.AsyncLangchainCallbackHandler()
 
     res = await chain.acall(message.content, callbacks=[cb])
-    answer = res["answer"]
+
+    answer = res["result"]
     source_documents = res["source_documents"]  # type: List[Document]
 
     text_elements = []  # type: List[cl.Text]
