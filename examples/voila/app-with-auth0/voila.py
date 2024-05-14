a65ab57efefc13add430947a47fc9110582efa35
neelasha23 <neelasha.sen@gmail.com>
2024-05-14 16:48:22 +0530

    voila auth0

---
+++ examples/voila/app-with-auth0/voila.py
@@ -0,0 +1,12 @@
+import nbformat
+
+
+def hook_function(req, notebook, cwd):
+   headers = req.request.headers
+   notebook.metadata.user = headers.get('X-Auth-Name', 'Anonymous')
+   with open("app.ipynb", 'w') as f:
+      nbformat.write(notebook, f)
+   return notebook
+
+
+c.Voila.prelaunch_hook = hook_function
