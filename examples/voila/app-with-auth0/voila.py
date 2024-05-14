728f6ed76858b41e5b5167219719cde0138f49ea
neelasha23 <neelasha.sen@gmail.com>
2024-05-14 18:44:21 +0530

    metadata update file

--- examples/voila/app-with-auth0/voila.py
+++ examples/voila/app-with-auth0/voila.py
@@ -4,7 +4,7 @@ import nbformat
 def hook_function(req, notebook, cwd):
    headers = req.request.headers
    notebook.metadata.user = headers.get('X-Auth-Name', 'Anonymous')
-   with open("app.ipynb", 'w') as f:
+   with open("app_updated.ipynb", 'w') as f:
       nbformat.write(notebook, f)
    return notebook
 
