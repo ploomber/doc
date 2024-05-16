0ec99ed3880f1bc55e62b23471ac519bb8f50f8b
neelasha23 <neelasha.sen@gmail.com>
2024-05-17 00:17:52 +0530

    New cell

--- examples/voila/app-with-auth0/voila.py
+++ examples/voila/app-with-auth0/voila.py
@@ -1,11 +1,13 @@
-import json
+import nbformat
 
 
 def hook_function(req, notebook, cwd):
    headers = req.request.headers
    user = headers.get('X-Auth-Name', 'Anonymous')
-   with open("authentication_data.json", 'w') as json_file:
-      json.dump({"user": user}, json_file)
+   set_user_cell = f"user = '{user}'\n"
+   print_user_cell = f"print(f\"Welcome {user}\")"
+   user_cell = nbformat.v4.new_code_cell(source=f"{set_user_cell}{print_user_cell}")
+   notebook.cells.insert(0, user_cell)
    return notebook
 
 
