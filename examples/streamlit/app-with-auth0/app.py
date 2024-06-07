03de0962062cc5a920d30d57bdc001502158dec4
Eduardo Blancas <github@blancas.io>
2024-06-07 09:38:39 -0600

    updates streamlit example

--- examples/streamlit/app-with-auth0/app.py
+++ examples/streamlit/app-with-auth0/app.py
@@ -6,10 +6,13 @@ headers = _get_websocket_headers()
 
 header_name = headers.get("X-Auth-Name")
 header_sub = headers.get("X-Auth-Sub")
-
+header_id_token = headers.get("X-Id-Token")
+header_access_token = headers.get("X-Access-Token")
 
 st.write(header_name)
 st.write(header_sub)
+st.write("Got ID token!" if header_id_token else "No ID token")
+st.write("Got Access token!" if header_access_token else "No Access token")
 
 # link to logout
 st.markdown('<a href="/exit" target="_self">Logout</a>', unsafe_allow_html=True)
