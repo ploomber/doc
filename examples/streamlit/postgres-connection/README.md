c976ea8d881363fbe74864390f75140d2785e35b
stanleyo <stanleyo@andrew.cmu.edu>
2024-07-25 11:12:01 -0400

    finalized app, update readme

--- examples/streamlit/postgres-connection/README.md
+++ examples/streamlit/postgres-connection/README.md
@@ -1,17 +1,10 @@
-# DB Connection 
+# Streamlit App Connected to Postgres
+To showcase the power of using a database with Streamlit apps to display live data, this app enables users to insert their own data into a table on their database. The new data is then automatically displayed and used in the app. 
 
-This is a sample app for demonstrating DB connections in Streamlit. To run this app ensure you set the `DB_URI` environment variable:
+![](app.png)
 
-```sh 
-export DB_URI=sqlite:///numbers.db
-```
-
-Note that while this example uses `SQLite` for simplicity, you can substitute the URI with the [SQLAlchemy URI](https://docs.sqlalchemy.org/en/20/core/engines.html#database-urls) corresponding to your specific database. Here's the format:
-
-```python
-dialect+driver://username:password@host:port/database
-```
+## Run App
+To run this app locally ensure you set the `DB_URI` variable in `app.py` to your personal database URI and that you have installed all the packages listed in requirements.txt.
 
 ## Deployment
-
 When deploying this app on Ploomber Cloud you need to set the `DB_URI` as a secret. Refer to the [documentation](https://docs.cloud.ploomber.io/en/latest/user-guide/secrets.html) to learn more.
--- examples/streamlit/postgres-connection/app.py
+++ examples/streamlit/postgres-connection/app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 from sqlalchemy import text, create_engine
 from os import environ
-import pandas as pd
 from ucimlrepo import fetch_ucirepo
-import psycopg2
 
 
-# Create the SQL connection to the numbers DB as specified in your secret.
-
-# Updates iris table with original
+# Updates iris table with the original dataset
 def upload_data(DB_URI):
     # Loading in iris dataset
     iris = fetch_ucirepo(name="Iris")
@@ -21,32 +17,38 @@ def upload_data(DB_URI):
         print("Data successfully uploaded.")
     engine.dispose()
 
+DB_URI = "YOUR_DB_URI"
 
-DB_URI = "postgresql://demo_owner:7iAYbGmr8eOJ@ep-lively-credit-a52udj7x.us-east-2.aws.neon.tech/demo?sslmode=require"
-# url=environ["DB_URI"]
+# If deploying on Ploomber Cloud, add DB_URI as a secret. 
+conn_st = st.connection(name="postgres", type='sql', url = environ["DB_URI"])
 
-conn_st = st.connection(name="postgres", type='sql', url=DB_URI)
+# If running locally
+# conn_st = st.connection(name="postgres", type='sql', url = DB_URI) 
 
+# App Creation
 st.title("Streamlit with Postgres Demo")
 iris_data = conn_st.query("SELECT * FROM iris")
 
+# Creates sidebar to add data
 with st.sidebar:    
     reset = st.button("Reset Data")
     st.header("Add Iris Data")
-    st.subheader("After submitting, a new row with the data will be added to the 'iris' table in our database.")
+    st.subheader("After submitting, a query is executed that inserts a new datapoint to the 'iris' table in our database.")
     with st.form(key='new_data'):
-        sepal_length = st.text_input(label="Sepal Length", key=1)
-        sepal_width = st.text_input(label="Sepal Length", key=2)
-        petal_length = st.text_input(label="Petal Length", key=3)
-        petal_width = st.text_input(label="Petal Width", key=4)
-        iris_class = st.selectbox(label="Iris Class", key=5, options=iris_data["class"].unique())
+        sepal_length = st.text_input(label="Sepal Length (cm)", key=1)
+        sepal_width = st.text_input(label="Sepal Length (cm)", key=2)
+        petal_length = st.text_input(label="Petal Length (cm)", key=3)
+        petal_width = st.text_input(label="Petal Width (cm)", key=4)
+        iris_class = st.selectbox(label="Iris Class (cm)", key=5, options=iris_data["class"].unique())
         submit = st.form_submit_button('Add')
 
+# Replaces dataset in database with original 
 if reset:
     upload_data(DB_URI)
     st.cache_data.clear()
     iris_data = conn_st.query("SELECT * FROM iris")
 
+# Inserts data into table
 if submit:
     with conn_st.session as s:
         new_data = (sepal_length, sepal_width, petal_length, petal_width, iris_class)
@@ -62,32 +64,33 @@ if submit:
             'iris_class': iris_class
         })
         s.commit()
+    # Clears the cached data so that streamlit fetches new data when updated. 
     st.cache_data.clear()
     iris_data = conn_st.query("SELECT * FROM iris")
 
-col1, col2 = st.columns(2)
+# Display Metrics
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    st.metric("Average Sepal Length", round(iris_data["sepal length"].mean(), 2))
+    st.metric("Average Sepal Length (cm)", round(iris_data["sepal length"].mean(), 2))
 with col2:
-    st.metric("Average Sepal Width", round(iris_data["sepal width"].mean(), 2))
-with col1:
-    st.metric("Average Petal Length", round(iris_data["petal length"].mean(), 2))
-with col2:
-    st.metric("Average Petal Width", round(iris_data["petal width"].mean(), 2))
+    st.metric("Average Sepal Width (cm)", round(iris_data["sepal width"].mean(), 2))
+with col3:
+    st.metric("Average Petal Length (cm)", round(iris_data["petal length"].mean(), 2))
+with col4:
+    st.metric("Average Petal Width (cm)", round(iris_data["petal width"].mean(), 2))
 
-st.divider()
-# Scatter plot selector
+# Displays Scatterplot
 st.header("Scatter Plot")
-with col1: 
-    x = st.selectbox("Select X-Variable", options=iris_data.select_dtypes("number").columns, index=0)
-with col2:
-    y = st.selectbox("Select Y-Variable", options=iris_data.select_dtypes("number").columns, index=1)
 
-# Scatter plot
+c1, c2 = st.columns(2)
+
+with c1: 
+    x = st.selectbox("Select X-Variable", options=iris_data.select_dtypes("number").columns, index=0)
+with c2:
+    y = st.selectbox("Select Y-Variable", options=iris_data.select_dtypes("number").columns, index=1)
 
 scatter_chart = st.scatter_chart(iris_data, x=x, y=y, size=40, color='class',)
 
-st.divider()
-
-st.dataframe(iris_data)
+# Displays Dataframe
+st.dataframe(iris_data, use_container_width=True)
